d351d9169a5f0ac3aebde0c8f4d7ea70011ed3a7
Ondrej Bruha <ondrej.bruha@outlook.com>
2023-05-24 10:16:50 +0200

    dalsi endpointy

--- server/controller/temperatureController.js
+++ server/controller/temperatureController.js
@@ -20,5 +20,54 @@ class TemperatureController {
             res.status(400).send(e);
         }
     }
+    async getTemperatureByDay(req, res){
+         try {
+             const body = req.body;
+             if(!body){
+                 throw new Error("Invalid body")
+             }
+             const date = new Date(body.date);
+             const records = await model.find();
+             let recordPerDay = [];
+             for(let r of records){
+                 let temp = new Date(r.recordDate);
+                 if(temp.getDay() === date.getDay() && temp.getMonth()===date.getMonth() && temp.getFullYear() === date.getFullYear()){
+                     recordPerDay.push(r);
+                 }
+             }
+             if(!recordPerDay.length){
+                 throw new Error("NO record");
+             }
+             res.json(recordPerDay);
+         }catch (e) {
+             res.status(400).send(e);
+         }
+
+    }
+    async getDateList(req, res){
+         let records = await model.find();
+         let out = [];
+         for(let r of records){
+             out.push(r.recordDate);
+         }
+         res.send(out);
+    }
+    async postNewRecord(req,res){
+         try{
+             const body = req.body;
+             if(!body){
+                 throw new Error("inavild Body");
+             }
+             if(!body.idStation && !body.temperature && !body.temperature[0]?.time && !body.temperature[0]?.value){
+                throw new Error("Inaviuld Body")
+             }
+             const result = new model(body);
+             res.json(body);
+         }catch (e) {
+            res.status(400).send(e);
+         }
+
+    }
+
 }
 module.exports = TemperatureController;
--- server/router/temperatureRouter.js
+++ server/router/temperatureRouter.js
@@ -4,5 +4,8 @@ const ControllerCls = require("../controller/temperatureController");
 const controller = new ControllerCls();
 
 router.get("/temperature", controller.getTemperature);
+router.get("/temperaturePerDay", controller.getTemperatureByDay);
+router.get("/dateList", controller.getDateList);
+router.post("/postNewRecord", controller.postNewRecord);
 
 module.exports = router;
