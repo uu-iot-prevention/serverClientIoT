ec2ae1609cdf86c65459900e777a426b5efd9fc8
LukasKrouzelViagem <krouzel@viagem.cz>
2023-05-28 04:59:28 +0200

    f
    f

--- client/src/components/AlertBox.jsx
+++ client/src/components/AlertBox.jsx
@@ -1,14 +1,42 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import Message from "./Message";
+import axios from "axios";
+import { useCookies } from 'react-cookie';
 
 const AlertBox = (props) => {
-    const {title,id} = props
-    return (
-        <div className="Alerts">
-            <h2>{title}</h2>
-            <h2>{id}</h2>
-            
-        </div>
-    )
-}
+    
+  const { title, id } = props;
+  const [data, setData] = useState([]);
+    const [cookies] = useCookies(["token"]);
+
+  const token = cookies?.token;
+
+  useEffect(() => {
+    const fetchData = async () => {
+    const headers = { Authorization: `Bearer ${token}` };
+      const response = await axios.get(`http://localhost:5003/station/alert-get/${id}`,{headers});
+      setData(response.data);
+      console.log(response.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="Alerts">
+        <h2 style={{marginLeft:'10px'}}><b>{title}</b></h2>
+      {data.map((data) => (
+        <Message 
+            key={data._id} 
+            id ={data._id} 
+            message={data.message} 
+            time ={data.time} 
+            typ = {data.type[0]}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default AlertBox;
---
+++ client/src/components/Message.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Alert, AlertTitle } from '@mui/material';
+import { Typography } from "@mui/material";
+
+const Message = ({ id, message, time,typ }) => {
+  let type="";
+  if(typ == "SOS"){
+    type = "error"
+  }else{
+    type = "warning"
+  }
+ 
+  return (
+    <Alert severity={type} style={{margin:'5px',borderRadius:'10px'}}>
+      <AlertTitle>{time}</AlertTitle>
+      
+        <Typography>{message}</Typography>
+        <Typography>{typ}</Typography>
+      
+    </Alert>
+  );
+};
+
+export default Message;
